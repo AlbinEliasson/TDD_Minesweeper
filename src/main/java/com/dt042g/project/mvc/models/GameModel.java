50549d573d309058251627b9ed855a7c3447cda1
makr1906 <makr1906@student.miun.se>
2023-04-20 02:40:42 +0200

    Implement board generation functionality in concrete model
    This commit partially implements the *GameModel* concrete implementation
    of the *Model* class. This adds functionality for generating the board
    and randomizing the placements of the mines; along with helper methods
    to achieve this. Specifically a method for getting all neighbors of a
    square and a implementation of the *Model.isMine()* method.
    
    The generation sequence is split into three parts:
    
    First the storage object is initialized, currently a two-dimensional
    list of *BackingSquare* objects is used. This is initialized using a
    length parameter provided in the object constructor, which decides the
    size of the board. Empty nested lists are then initialized to form the
    full storage object.
    
    Second the *BackingSquare* objects are initialized. Each possible
    location on the board is iterated through and a new *BackingSquare*
    instance is created. Then a random number of generated, and together
    with a predefined constant *_mineChance*, whether the square is a mine
    is set. This step is repeated for every *BackingSquare*.
    
    Lastly, a "first location" parameter is passed to the generation method.
    This location is used to ensure that the first square that is clicked is
    not a mine, which would cause the user to loose unfairly. To prevent
    this a check is run to ensure that the first clicked square, and its
    direct neighbors are never allowed to be mines. This results in a
    2x2 - 3x3 area always being opened upon first click giving the user a
    fair shot.

---
+++ src/main/java/com/dt042g/project/mvc/models/GameModel.java
@@ -0,0 +1,179 @@
+package com.dt042g.project.mvc.models;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class GameModel extends Model {
+    private final double _mineChance = 0.15;
+    private final int _boardSize;
+    private List<List<BackingSquare>> _board;
+
+    public GameModel(int boardSize) {
+        _boardSize = boardSize;
+        _board = null;
+    }
+
+    /**
+     * Method for (re)generating every square on the board.
+     */
+    private void generateSquares(Point firstLocation)  {
+        _board = new ArrayList<>();
+        Random random = new Random();
+
+        // Add "rows" to board
+        for(int i = _board.size(); i < _boardSize; i++)
+            _board.add(i, new ArrayList<>());
+
+        // Iterate through each "row"
+        for(List<BackingSquare> row : _board) {
+            // Iterate through each square in row
+            for(int i = 0; i < _boardSize; i++) {
+                // Create a new backing square
+                row.add(i, new BackingSquare());
+
+                // Randomize whether the square is a mine
+                boolean isMine = random.nextDouble() < _mineChance;
+
+                // Set whether it is a mine
+                row.get(i).setMine(isMine);
+            }
+        }
+
+        // Skip "first square protection" if first location is null
+        if(firstLocation == null)
+            return;
+
+        // The first square clicked is a mine, make it not a mine.
+        if(isMine(firstLocation)) {
+            _board.get(firstLocation.x).get(firstLocation.y).setMine(false);
+        }
+
+        // Check each neighbour of the first clicked square.
+        for(Point neighbor : getNeighbors(firstLocation)) {
+            // If the neighbor is a mine, make it not a mine.
+            if(isMine(neighbor)) {
+                _board.get(neighbor.x).get(neighbor.y).setMine(false);
+            }
+        }
+    }
+
+    /**
+     * Method for getting all the neighbor squares of a location.
+     *
+     * @param location The location of the subject.
+     *
+     * @return The neighbors.
+     */
+    private List<Point> getNeighbors(Point location) {
+        List<Point> neighbors = new ArrayList<>();
+
+        if(location == null)
+            return neighbors;
+
+        if(location.x > 0)
+            neighbors.add(new Point(location.x - 1, location.y)); // West
+        if(location.x < _boardSize - 1)
+            neighbors.add(new Point(location.x + 1, location.y)); // East
+        if(location.y > 0)
+            neighbors.add(new Point(location.x, location.y - 1)); // North
+        if(location.y < _boardSize - 1)
+            neighbors.add(new Point(location.x, location.y + 1)); // South
+        if(location.x > 0 && location.y > 0)
+            neighbors.add(new Point(location.x - 1, location.y - 1)); // North-west
+        if(location.x < _boardSize - 1 && location.y < _boardSize - 1)
+            neighbors.add(new Point(location.x + 1, location.y + 1)); // South-east
+        if(location.x > 0 && location.y < _boardSize - 1)
+            neighbors.add(new Point(location.x - 1, location.y + 1)); // South-west
+        if(location.x < _boardSize - 1 && location.y > 0)
+            neighbors.add(new Point(location.x + 1, location.y - 1)); // North-east
+
+        return neighbors;
+    }
+
+    /**
+     * Method for getting the number of neighboring mines to a square.
+     *
+     * @param location The location of the square to check.
+     * @return The number of neighboring mines.
+     * @throws IndexOutOfBoundsException If the location is outside the size of the board.
+     */
+    @Override
+    public int getSquareValue(Point location) {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    /**
+     * Method for checking if a square is currently flagged.
+     *
+     * @param location The location of the square to check.
+     * @return Whether the square is flagged.
+     * @throws IndexOutOfBoundsException If the location is outside the size of the board.
+     */
+    @Override
+    public boolean isFlagged(Point location) {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    /**
+     * Method for checking if a square is a mine.
+     *
+     * @param location The location of the square to check.
+     *
+     * @return Whether the square is a mine.
+     *
+     * @throws IndexOutOfBoundsException If the location is outside the size of the board.
+     * @throws NullPointerException If the location is null.
+     */
+    @Override
+    public boolean isMine(Point location) {
+        if(_board == null)
+            return false;
+
+        return _board.get(location.x).get(location.y).isMine();
+    }
+
+    /**
+     * Method for checking if a square is revealed.
+     *
+     * @param location The location of the square to check.
+     * @return Whether the square is revealed.
+     * @throws IndexOutOfBoundsException If the location is outside the size of the board.
+     */
+    @Override
+    public boolean isRevealed(Point location) {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    /**
+     * Method for selecting a square to be revealed.
+     *
+     * @param location The location of the square to reveal.
+     * @throws IndexOutOfBoundsException If the location is outside the size of the board.
+     */
+    @Override
+    public void selectSquare(Point location) {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    /**
+     * Method for setting the flagged value of a square.
+     *
+     * @param location The location of the square to flag.
+     * @param value    The flag value; true = flagged, false = not flagged.
+     * @throws IndexOutOfBoundsException If the location is outside the size of the board.
+     */
+    @Override
+    public void setSquareFlag(Point location, boolean value) {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+
+    /**
+     * Method for resetting the backing game board; I.E. restarting the game.
+     */
+    @Override
+    public void reset() {
+        throw new UnsupportedOperationException("Not implemented!");
+    }
+}
---
+++ src/test/java/com/dt042g/project/mvc/models/gamemodel/GameModelKnownBoardTests.java
@@ -0,0 +1,96 @@
+package com.dt042g.project.mvc.models.gamemodel;
+
+import com.dt042g.project.mvc.models.BackingSquare;
+import com.dt042g.project.mvc.models.GameModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.awt.Point;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * Class containing "known board" unit-tests for the *GameModel* class.
+ *
+ * @author Martin K. Herkules (makr1906) & Albin Eliasson (alel2104)
+ */
+public class GameModelKnownBoardTests {
+    /*
+     * A template for a known board.
+     *
+     * Each cell corresponds to a square in the model. The first character
+     * decides revealed(R)/flagged(F)/hidden(H). And the second its value, M is
+     * for mines; and a number is for a "value square", with the number being
+     * the exact number of neighboring squares.
+     */
+    private static final String[][] boardTemplate = new String[][] {
+            { "H0", "H1", "HM", "R2", "FM" },   // (Hidden Zero     ), (Hidden One      ), (Hidden Mine     ), (Revealed Two    ), (Flagged Mine    )
+            { "H2", "H3", "R2", "R2", "R1" },   // (Hidden Two      ), (Hidden Three    ), (Revealed Two    ), (Revealed Two    ), (Revealed One    )
+            { "HM", "HM", "H1", "H0", "H0" },   // (Hidden Mine     ), (Hidden Mine     ), (Hidden One      ), (Hidden Zero     ), (Hidden Zero     )
+            { "H2", "H2", "F2", "H2", "H2" },   // (Hidden Two      ), (Hidden Two      ), (Flagged Two     ), (Hidden Two      ), (Hidden Two      )
+            { "H0", "H0", "H1", "HM", "FM" }    // (Hidden Zero     ), (Hidden Zero     ), (Hidden One      ), (Hidden Mine     ), (Flagged Mine    )
+    };
+
+    private static Field fieldBoard;
+
+    /**
+     * Argument generator method for running tests with all possible locations.
+     *
+     * @return Argument with a model and board size.
+     */
+    private static Stream<Arguments> Location() {
+        return IntStream.range(0, boardTemplate.length).boxed()
+                .flatMap(x -> IntStream.range(0, boardTemplate.length)
+                        .mapToObj(y -> Arguments.of(new Point(x, y))));
+    }
+
+    private GameModel model;
+
+    /**
+     * Method which is triggered before any test method is executed.
+     */
+    @BeforeAll
+    public static void setupAll() throws NoSuchFieldException {
+        fieldBoard = GameModel.class.getDeclaredField("_board");
+        fieldBoard.setAccessible(true);
+    }
+
+    /**
+     * Method which is triggered before each individual test method is executed.
+     */
+    @BeforeEach
+    public void setupEach() throws IllegalAccessException {
+        model = new GameModel(boardTemplate.length);
+
+        // Parse board template into backing squares
+        List<List<BackingSquare>> board = Arrays.stream(boardTemplate)
+                        .map(row -> Arrays.stream(row).map(cell -> {
+                            BackingSquare square = new BackingSquare();
+                            square.setMine(cell.charAt(1) == 'M');
+                            square.setRevealed(cell.charAt(0) == 'R');
+                            square.setFlagged(cell.charAt(0) == 'F');
+                            return square;
+                        }).toList()).toList();
+
+        fieldBoard.set(model, board);
+    }
+
+    /**
+     * Method for checking the isMine method to ensure that correct values are
+     * returned.
+     *
+     * @param location Location to check.
+     */
+    @ParameterizedTest
+    @MethodSource("Location")
+    public void test_IsMine_CorrectReturn(Point location) {
+        Assertions.assertEquals(boardTemplate[location.x][location.y].charAt(1) == 'M', model.isMine(location));
+    }
+}
