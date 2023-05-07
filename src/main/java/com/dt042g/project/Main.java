9672de34579837b2797d7ac9813d1c04737a4617
makr1906 <makr1906@student.miun.se>
2023-05-07 04:49:18 +0200

    Implement main method
    This commit implements the main method to instantiate a set of MVC
    components. The components are initialized using a predefined board
    size; and are called using the *invokeLater* method to ensure.
    
    Creating the instances on using *invokeLater* ensures exactly all GUI
    modifying calls are run on the main event thread. And since most calls
    will be made on the GUI thread (I.E. any call chain started by a user
    input event on the GUI) there will be negligible unnecessary load on the
    event thread.

--- src/main/java/com/dt042g/project/Main.java
+++ src/main/java/com/dt042g/project/Main.java
@@ -1,12 +1,17 @@
 package com.dt042g.project;
 
+import com.dt042g.project.mvc.controllers.GameController;
+import com.dt042g.project.mvc.models.GameModel;
+import com.dt042g.project.mvc.views.GameView;
+
+import javax.swing.SwingUtilities;
+
 /**
  * Class containing main entry point of the Minesweeper application.
  *
  * @author Martin K. Herkules (makr1906) & Albin Eliasson (alel2104)
  */
 public class Main {
-
     /**
      * com.dt042g.project.Main entry point of the Minesweeper application.
      *
@@ -14,5 +19,8 @@ public class Main {
      */
     public static void main(String... args) {
         System.out.println("Minesweeper!");
+
+        final int boardSize = 20;
+        SwingUtilities.invokeLater(() -> new GameController(new GameModel(boardSize), new GameView(boardSize)));
     }
 }
